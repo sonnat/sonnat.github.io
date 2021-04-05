015997f0bd0c30f4228b4fae28d1f4fddbe276a9
Mostafa Shamsitabar <mostafa.shamsitabar@cafebazaar.ir>
2021-04-05 17:10:02 +0430

    [:art:] added burger and page states

--- store.ts
+++ store.ts
@@ -3,13 +3,21 @@ import { persist } from "zustand/middleware";
 
 interface IState extends State {
   isDarkMode: boolean;
+  isBurgerMenuOpen: boolean;
+  isPageLoading: boolean;
   toggleDarkMode: () => void;
+  setPageLoading: (isLoading: boolean) => void;
+  setBurgerMenuOpen: (isOpen: boolean) => void;
 }
 
 const useStore = createStore<IState>(
   persist(
     set => ({
       isDarkMode: false,
+      isBurgerMenuOpen: false,
+      isPageLoading: false,
+      setPageLoading: isLoading => set(() => ({ isPageLoading: isLoading })),
+      setBurgerMenuOpen: isOpen => set(() => ({ isBurgerMenuOpen: isOpen })),
       toggleDarkMode: () => set(state => ({ isDarkMode: !state.isDarkMode }))
     }),
     { name: "theme-mode-storage", whitelist: ["isDarkMode"] }
